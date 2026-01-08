f9bf5995afc9f75308b2dcd2d72bf23a0dc6bce1
Edwin Kantor-Tomcek <77319863+Edwin-KT@users.noreply.github.com>
2026-01-08 22:11:52 +0200

    remove

--- TheGame/Server/Config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cstdint>
-
-namespace Config {
-    inline constexpr uint16_t PORT = 18080;
-}
--- TheGame/Server/Database.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-// Acest fisier este un placeholder pentru organizarea proiectului.
